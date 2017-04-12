dc9b25a03ea98d02ae9ea3507bab9f7e8eb28110
Ginger Williams <gwilliams@Ginger-Williamss-MacBook.local>
2017-04-11 20:46:51 -0400

    [GW] Neact Noob init

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function () {
+    return gulp.src('css/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(cleanCSS({keepSpecialComments : 0}))
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('default', function (){
+    gulp.watch('css/scss/**/*.scss', ['sass'], ['minify-css']);
+});
