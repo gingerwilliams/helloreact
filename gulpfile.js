5724ebfea5bafb897ba328edecf7b31530b5257b
Ginger Williams <gwilliams@Ginger-Williamss-MacBook.local>
2017-04-18 17:06:33 -0400

    [GW] ToDo App set up

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
-    return gulp.src('css/scss/**/*.scss')
+    return gulp.src('public/css/scss/**/*.scss')
         .pipe(sass())
         .pipe(cleanCSS({keepSpecialComments : 0}))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('public/css'))
 });
 
 gulp.task('default', function (){
-    gulp.watch('css/scss/**/*.scss', ['sass'], ['minify-css']);
+    gulp.watch('public/css/scss/**/*.scss', ['sass'], ['minify-css']);
 });
